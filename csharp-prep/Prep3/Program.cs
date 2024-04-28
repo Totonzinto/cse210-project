911c6caaee3659177c9c71d7e7313b00b8f78dd2
totonzinto <chiomanwosu67@gmail.com>
2024-04-28 02:41:36 +0100

    added code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenator = new Random();
+        int magicNumber = randomGenator.Next(1, 101);
+        int guess;
+
+        while (true)
+        {
+            Console.WriteLine("what is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Guess higher!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Guess lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! The magic number was " + magicNumber);
+            break;
+            }
+        }
     }
 }
+
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Initialize an empty list
+        List<int> numbers = new List<int>();
+
+        // Ask the user for a series of numbers
+        while (true)
+        {
+            Console.Write("Enter a number (0 to stop): ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+            {
+                break;
+            }
+            numbers.Add(num);
+        }
+
+        // Compute the sum, average, and max of the numbers in the list
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        double average = (double)total / numbers.Count;
+        int maxNum = numbers.Max();
+        // Print the results
+        Console.WriteLine("Sum: " + total);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Max: " + maxNum);
     }
 }
+
+
+
